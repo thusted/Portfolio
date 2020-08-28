dac87f467bbdaba6db7e77f6d30655521f6a1ea1
Tiana Husted <tiana.husted@gmail.com>
2020-08-28 13:28:49 -0700

    First commit with new materialize elements

--- README.md
+++
@@ -1,4 +0,0 @@
-# Portfolio
-This is a portfolio designed to showcase work I have done as a web developer.
-
-Link to Deployed Application: https://thusted.github.io/Portfolio/
---
+++ assets/js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.carousel').carousel();
+    $('.materialboxed').materialbox();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
